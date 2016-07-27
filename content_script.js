7bf180fbd9455b14a827b290af68cc2e160c4387
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-07-28 00:35:46 +0900

    [Clean] 変数定義を可能な限りlet -> constに変更

--- content_script.js
+++ content_script.js
@@ -21,7 +21,7 @@ if (videos.length === 1) {
 	// ホイールで音量変更
 	// 下から40pxの領域ではシークする
 	const seekAreaHeight = 40;
-	let changeVolume = diff => {
+	const changeVolume = diff => {
 		// volume: [0, 1]
 		let vol = video.volume;
 		vol += diff;
@@ -29,13 +29,13 @@ if (videos.length === 1) {
 		vol = Math.max(vol, 0);
 		video.volume = vol;
 	};
-	let seek = diff => {
+	const seek = diff => {
 		video.currentTime += diff;
 	};
 	video.addEventListener("wheel", evt => {
 		// 下方向へのスクロール: 正
-		let toBottom = evt.deltaY > 0;
-		let pxFromBottom = video.clientHeight - evt.offsetY;
+		const toBottom = evt.deltaY > 0;
+		const pxFromBottom = video.clientHeight - evt.offsetY;
 		if (pxFromBottom < seekAreaHeight) {
 			seek(5 * (toBottom ? 1 : -1));
 		} else {
@@ -44,9 +44,9 @@ if (videos.length === 1) {
 	});
 
 	// ダブルクリックで最大化/最大化解除
-	let isFullScreen = () => document.webkitIsFullScreen;
-	let requestFullScreen = elem => elem.webkitRequestFullScreen();
-	let cancelFullScreen = () => document.webkitCancelFullScreen();
+	const isFullScreen = () => document.webkitIsFullScreen;
+	const requestFullScreen = elem => elem.webkitRequestFullScreen();
+	const cancelFullScreen = () => document.webkitCancelFullScreen();
 	video.addEventListener("dblclick", evt => {
 		// TODO: ダブルクリック時に一時停止・再生がされないようにする
 		// 		（clickイベントの中でsetTimeoutを使ってうまくやる）
